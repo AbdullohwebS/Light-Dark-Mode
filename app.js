361525ec2213b51f7ea6707ebc6a66c12c2e144f
AbdullohwebS <abdullohnurmuhammedov654@gmail.com>
2025-02-15 11:11:34 +0500

    LIGHT DARK

---
+++ app.js
@@ -0,0 +1,22 @@
+const darkModeToggle = document.querySelector(".dark img");
+const body = document.body;
+
+const darkImg = "images/Dark.png";
+const lightImg = "images/Light.png";
+
+if (localStorage.getItem("theme") === "light") {
+  body.classList.add("light-mode");
+  darkModeToggle.src = lightImg;
+}
+
+darkModeToggle.addEventListener("click", () => {
+  if (body.classList.contains("light-mode")) {
+    body.classList.remove("light-mode");
+    darkModeToggle.src = darkImg;
+    localStorage.setItem("theme", "dark");
+  } else {
+    body.classList.add("light-mode");
+    darkModeToggle.src = lightImg;
+    localStorage.setItem("theme", "light");
+  }
+});
